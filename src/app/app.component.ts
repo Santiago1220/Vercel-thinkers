ecde98aa9bd1f7cd0a5e0321a55e88d5f8bfee35
Santiago Restrepo <santiago.restrepo@ceiba.com.co>
2023-02-15 07:20:04 -0500

    Se realiza desarrollo para generar contraseñas en ceiba

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,60 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'vercel-project';
+  lengthField = new FormControl(Validators.minLength(8));
+  length = 8;
+  includeLetters = false;
+  includeNumbers = false;
+  includeSymbols = false;
+  password = '';
+  showInfo = false;
+  onCopyAlert = false;
+
+
+  toggleInfo() {
+    this.showInfo = !this.showInfo;
+  }
+
+  onCopyText() {
+    this.onCopyAlert = !this.onCopyAlert;
+  }
+
+  onChangeUseLetters() {
+    this.includeLetters = !this.includeLetters;
+  }
+  onChangeUseNumbers() {
+    this.includeNumbers = !this.includeNumbers;
+  }
+  onChangeUseSymbols() {
+    this.includeSymbols = !this.includeSymbols;
+  }
+
+  onButtonClick() {
+    const numbers = '1234567890';
+    const letters = 'abcdefghijklmnñopqrstuvwxyz';
+    const symbols = '!#$%&/()=?¡¿*@';
+
+    let validChars = '';
+    if(this.includeLetters) {
+      validChars += letters;
+    }
+    if(this.includeNumbers) {
+      validChars += numbers;
+    }
+    if(this.includeSymbols) {
+      validChars += symbols;
+    }
+
+    let generatedPassword = '';
+    for(let i= 0; i<this.length; i++){
+      const index = Math.floor(Math.random() * validChars.length);
+      generatedPassword += validChars[index];
+    }
+    this.password = generatedPassword;
+  }
+
+  onCopyPass(){
+    navigator.clipboard.writeText(this.password);
+    this.onCopyText();
+  }
 }
